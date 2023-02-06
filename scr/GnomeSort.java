8a5ce4e750c951d7fb6c7a132020ff6b96dbd58c
LeoConQui <con18797@uvg.edu.gt>
2023-02-06 16:39:24 -0600

    Modificacion del codigo de gnomesort

--- scr/GnomeSort.java
+++ scr/GnomeSort.java
@@ -1,30 +1,41 @@
 package scr;
+import java.util.ArrayList;
+
 
 /**
- * @author obtenido desde https://www.geeksforgeeks.org/java-program-for-gnome-sort/ 
+ * @author Leonel Contreras 18797
+ * obtenido desde https://www.geeksforgeeks.org/java-program-for-gnome-sort/ 
  * @version 1.0
  */
 
 public class GnomeSort {
-    
-    public static void gnomeSort(int arr[], int n)
+    /** gnomeSort es un metodo que implementa el algoritmo de gnome sort para ordenar un arraylist de enteros
+     * Se declara static para poder utilizarlo sin necesidad de crear una instancia de la clase
+     * @param listaaordenar es el arraylist de enteros que se desea ordenar
+     */
+    public static void gnomeSort(ArrayList<Integer> listaaordenar)
         {
         int index = 0;
+        int n = listaaordenar.size();
+        int temp;
         
+        // realizamos el procedimiento mientras el indice sea menor al tamano del array
         while (index < n) {
-        if (index == 0)
-            index++;
-        if (arr[index] >= arr[index - 1])
-            index++;
-        else {
-            int temp = 0;
-            temp = arr[index];
-            arr[index] = arr[index - 1];
-            arr[index - 1] = temp;
+            // validamos si el indice es igual a cero
+            if (index == 0){
+                index++;
+            }
+
+            if (listaaordenar.get(index)>= listaaordenar.get(index-1)){
+                index++;
+            }else {
+            temp = listaaordenar.get(index);         
+            listaaordenar.set(index,listaaordenar.get(index-1));
+            listaaordenar.set(index-1, temp);            
             index--;
+            }
         }
-        }
-        return;
+        //return;
         }
 
 }
